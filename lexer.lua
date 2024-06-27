1ad4c6ef0cb7dd04c459d19608bf04c67a3c54e2
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2024-06-27 09:43:27 -0300

    update | lexer

--- lexer.lua
+++ lexer.lua
@@ -1,17 +1,17 @@
 -- MIT License
--- 
+--
 -- Copyright (c) 2018 LoganDark
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
 -- in the Software without restriction, including without limitation the rights
 -- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 -- copies of the Software, and to permit persons to whom the Software is
 -- furnished to do so, subject to the following conditions:
--- 
+--
 -- The above copyright notice and this permission notice shall be included in all
 -- copies or substantial portions of the Software.
--- 
+--
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,412 +20,297 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
-local function lookupify(src, list)
-	list = list or {}
+local function lookupify(src)
+  local list = {}
 
-	if type(src) == 'string' then
-		for i = 1, src:len() do
-			list[src:sub(i, i)] = true
-		end
-	elseif type(src) == 'table' then
-		for i = 1, #src do
-			list[src[i]] = true
-		end
-	end
+  if type(src) == 'string' then
+    for i = 1, src:len() do
+      list[src:sub(i, i)] = true
+    end
+  elseif type(src) == 'table' then
+    for i = 1, #src do
+      list[src[i]] = true
+    end
+  end
 
-	return list
+  return list
 end
 
-local base_ident = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
-local base_digits = '0123456789'
-local base_operators = '+-*/^%#'
+local alphabet = lookupify('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_')
+local whitespace = lookupify(' \n\t\r')
 
-local chars = {
-	whitespace = lookupify(' \n\t\r'),
-	validEscapes = lookupify('abfnrtv"\'\\'),
-	ident = lookupify(
-		base_ident .. base_digits,
-		{
-			start = lookupify(base_ident),
-		}
-	),
+local digits = lookupify('0123456789')
+local hex_digits = lookupify('0123456789abcdefABCDEF')
+local scientific_digits = lookupify(".Ee+-")
 
-	digits = lookupify(
-		base_digits,
-		{
-			hex = lookupify(base_digits .. 'abcdefABCDEF')
-		}
-	),
-
-	symbols = lookupify(
-		base_operators .. ',{}[]();.:', {
-			equality = lookupify('~=><'),
-			operators = lookupify(base_operators)
-		}
-	)
-}
+local symbols = lookupify("[]{}():;,")
+local operators = lookupify('+-*/%^~=><')
+local arithmetic_operators = lookupify('+-*/%^')
+local relational_operators = lookupify('~=><')
 
 local keywords = {
-	structure = lookupify({
-		'and', 'break', 'do', 'else', 'elseif', 'end', 'for', 'function',
-		'goto', 'if', 'in', 'local', 'not', 'or', 'repeat', 'return', 'then',
-		'until', 'while'
-	}),
+  structure = lookupify({
+    'and', 'break', 'do', 'else', 'elseif', 'end', 'for', 'function',
+    'goto', 'if', 'in', 'local', 'not', 'or', 'repeat', 'return', 'then',
+    'until', 'while', '<const>'
+  }),
 
-	values = lookupify({
-		'true', 'false', 'nil'
-	})
+  value = lookupify({
+    'true', 'false', 'nil'
+  })
 }
 
-return function(text)
-	local pos = 1
-	local start = 1
-	local buffer = {}
-	local lines = {}
-
-	local function look(delta)
-		delta = pos + (delta or 0)
-
-		return text:sub(delta, delta)
-	end
-
-	local function get()
-		pos = pos + 1
-
-		return look(-1)
-	end
-
-	local function getDataLevel()
-		local num = 0
-
-		while look(num) == '=' do
-			num = num + 1
-		end
-
-		if look(num) == '[' then
-			pos = pos + num + 1
-
-			return num
-		end
-	end
-
-	local function getCurrentTokenText()
-		return text:sub(start, pos - 1)
-	end
-
-	local currentLineLength = 0
-	local lineoffset = 0
-
-	local function pushToken(type, text)
-		text = text or getCurrentTokenText()
-
-		local tk = buffer[#buffer]
-
-		if not tk or tk.type ~= type then
-			tk = {
-				type = type,
-				data = text,
-				posFirst = start - lineoffset,
-				posLast = pos - 1 - lineoffset
-			}
-
-			if tk.data ~= '' then
-				buffer[#buffer + 1] = tk
-			end
-		else
-			tk.data = tk.data .. text
-			tk.posLast = tk.posLast + text:len()
-		end
-
-		currentLineLength = currentLineLength + text:len()
-		start = pos
-
-		return tk
-	end
-
-	local function newline()
-		lines[#lines + 1] = buffer
-		buffer = {}
-
-		get()
-		pushToken('newline')
-		buffer[1] = nil
-
-		lineoffset = lineoffset + currentLineLength
-		currentLineLength = 0
-	end
-
-	local function getData(level, type)
-		while true do
-			local char = get()
-
-			if char == '' then
-				return
-			elseif char == '\n' then
-				pos = pos - 1
-				pushToken(type)
-				newline()
-			elseif char == ']' then
-				local valid = true
-
-				for i = 1, level do
-					if look() == '=' then
-						pos = pos + 1
-					else
-						valid = false
-						break
-					end
-				end
-
-				if valid and look() == ']' then
-					pos = pos - level - 1
-
-					return
-				end
-			end
-		end
-	end
-
-	local function chompWhitespace()
-		while true do
-			local char = look()
-
-			if char == '\n' then
-				pushToken('whitespace')
-				newline()
-			elseif chars.whitespace[char] then
-				pos = pos + 1
-			else
-				break
-			end
-		end
-
-		pushToken('whitespace')
-	end
-
-	while true do
-		chompWhitespace()
-
-		local char = get()
-
-		if char == '' then
-			break
-		elseif char == '-' and look() == '-' then
-			pos = pos + 1
-
-			if look() == '[' then
-				pos = pos + 1
-
-				local level = getDataLevel()
-
-				if level then
-					getData(level, 'comment')
-
-					pos = pos + level + 2
-					pushToken('comment')
-				else
-					while true do
-						local char2 = get()
-
-						if char2 == '' or char2 == '\n' then
-							pos = pos - 1
-							pushToken('comment')
-
-							if char2 == '\n' then
-								newline()
-							end
-
-							break
-						end
-					end
-				end
-			else
-				while true do
-					local char2 = get()
-
-					if char2 == '' or char2 == '\n' then
-						pos = pos - 1
-						pushToken('comment')
-
-						if char2 == '\n' then
-							newline()
-						end
-
-						break
-					end
-				end
-			end
-
-			pushToken('comment')
-		elseif char == '\'' or char == '"' then
-			pushToken('string_start')
-
-			while true do
-				local char2 = get()
-
-				if char2 == '\\' then
-					pos = pos - 1
-					pushToken('string')
-					get()
-
-					local char3 = get()
-
-					if chars.digits[char3] then
-						for i = 1, 2 do
-							if chars.digits[look()] then
-								pos = pos + 1
-							end
-						end
-					elseif char3 == 'x' then
-						if chars.digits.hex[look()] and chars.digits.hex[look(1)] then
-							pos = pos + 2
-						else
-							pushToken('unidentified')
-						end
-					elseif char3 == '\n' then
-						pos = pos - 1
-						pushToken('escape')
-						newline()
-					elseif not chars.validEscapes[char3] then
-						pushToken('unidentified')
-					end
-
-					pushToken('escape')
-				elseif char2 == '\n' then
-					pos = pos - 1
-					pushToken('string')
-					newline()
-
-					break
-				elseif char2 == char or char2 == '' then
-					pos = pos - 1
-					pushToken('string')
-					get()
-
-					break
-				end
-			end
-
-			pushToken('string_end')
-		elseif chars.ident.start[char] then
-			while chars.ident[look()] do
-				pos = pos + 1
-			end
-
-			local word = getCurrentTokenText()
-
-			if keywords.structure[word] then
-				pushToken('keyword')
-			elseif keywords.values[word] then
-				pushToken('value')
-			else
-				pushToken('ident')
-			end
-		elseif chars.digits[char] or (char == '.' and chars.digits[look()]) then
-			if char == '0' and look() == 'x' then
-				pos = pos + 1
-
-				while chars.digits.hex[look()] do
-					pos = pos + 1
-				end
-			else
-				while chars.digits[look()] do
-					pos = pos + 1
-				end
-
-				if look() == '.' then
-					pos = pos + 1
-
-					while chars.digits[look()] do
-						pos = pos + 1
-					end
-				end
-
-				if look():lower() == 'e' then
-					pos = pos + 1
-
-					if look() == '-' then
-						pos = pos + 1
-					end
-
-					while chars.digits[look()] do
-						pos = pos + 1
-					end
-				end
-			end
-
-			pushToken('number')
-		elseif char == '[' then
-			local level = getDataLevel()
-
-			if level then
-				pushToken('string_start')
-
-				getData(level, 'string')
-				pushToken('string')
-
-				pos = pos + level + 2
-				pushToken('string_end')
-			else
-				pushToken('symbol')
-			end
-		elseif char == '.' then
-			if look() == '.' then
-				pos = pos + 1
-
-				if look() == '.' then
-					pos = pos + 1
-				end
-			end
-
-			if getCurrentTokenText():len() == 3 then
-				pushToken('vararg')
-			else
-				pushToken('symbol')
-			end
-		elseif char == ':' and look() == ':' then
-			get()
-
-			pushToken('label_start')
-
-			chompWhitespace()
-
-			if chars.ident.start[look()] then
-				get()
-
-				while chars.ident[look()] do
-					get()
-				end
-
-				pushToken('label')
-
-				chompWhitespace()
-
-				if look() == ':' and look(1) == ':' then
-					get()
-					get()
-
-					pushToken('label_end')
-				end
-			end
-		elseif chars.symbols.equality[char] then
-			if look() == '=' then
-				pos = pos + 1
-			end
-
-			pushToken('operator')
-		elseif chars.symbols[char] then
-			if chars.symbols.operators[char] then
-				pushToken('operator')
-			else
-				pushToken('symbol')
-			end
-		else
-			pushToken('unidentified')
-		end
-	end
-
-	lines[#lines + 1] = buffer
-
-	return lines
+return function(src, opts)
+  opts = opts or {}
+  opts.extended_assignment = opts.extended_assignment or false
+
+  local tokens = {}
+
+  local pos = 1
+  local start = 1
+  local lineoffset = 0
+  local currentLineLength = 0
+
+  local function look(delta)
+    delta = pos + (delta or 0)
+    return src:sub(delta, delta)
+  end
+
+  local function get()
+    pos = pos + 1
+    return look(-1)
+  end
+
+  local function pushToken(type, data)
+    data = data or src:sub(start, pos - 1)
+
+    local token = {
+      type = type,
+      data = data,
+      posFirst = start - lineoffset,
+      posLast = pos - 1 - lineoffset
+    }
+
+    if token.data ~= '' or token.type == "newline" then
+      table.insert(tokens, token)
+    end
+
+    currentLineLength = currentLineLength + data:len()
+    start = pos
+
+    return token
+  end
+
+  local function newline(push)
+    while look() == "\n" do
+      get()
+    end
+
+    start = pos
+
+    if push then
+      pushToken('newline')
+    end
+
+    lineoffset = lineoffset + currentLineLength
+    currentLineLength = 0
+  end
+
+  local function chompWhitespace()
+    while true do
+      local char = look()
+
+      if char == '\n' then
+        newline(true)
+      elseif whitespace[char] then
+        get()
+      else
+        break
+      end
+    end
+
+    start = pos
+  end
+
+  local function chompComment()
+    while true do
+      local char = look()
+
+      if char == "-" and look(1) == '-' then
+        local is_block_comment = look(2) == "[" and look(3) == "["
+        while true do
+          local c = look()
+
+          if (is_block_comment and c == "]" and look(-1) == "]") or (not is_block_comment and look(1) == '\n') then
+            get()
+            newline(true)
+            break
+          end
+
+          get()
+        end
+      else
+        break
+      end
+    end
+  end
+
+  local tokenizer = {
+    ['string'] = function(init)
+      local level = 0
+
+      if init == '[' then
+        while look() == '=' do
+          level = level + 1
+          get()
+        end
+        get()
+      end
+
+      pushToken("string:start")
+      -----------------------------
+      while true do
+        local char = get()
+        local next_char = look()
+
+        if init ~= '[' and next_char == init and char ~= '\\' then
+          break
+        end
+
+        if init == '[' and next_char == ']' and look(level + 1) == ']' then
+          break
+        end
+
+        if pos == #src then
+          error("The program failed to identify the string closure token: " .. tokens[#tokens].data:gsub('%[', ']'))
+        end
+      end
+
+      pushToken('string')
+      -----------------------------
+      get()
+      if init == '[' then
+        while look() == '=' do
+          get()
+        end
+        get()
+      end
+
+      pushToken("string:end")
+    end,
+    ['word'] = function()
+      local token = look(-1)
+
+      while alphabet[look()] or digits[look()] do
+        token = token .. get()
+      end
+
+      if keywords.structure[token] then
+        pushToken('keyword')
+      elseif keywords.value[token] then
+        pushToken((token == 'true' or token == 'false') and 'boolean' or 'nil')
+      elseif tokens[#tokens].data == "goto" or tokens[#tokens].data == '::' then
+        pushToken('label')
+      else
+        pushToken('identifier')
+      end
+    end,
+    ['number'] = function()
+      if look(-1) == '0' and look() == 'x' then
+        get()
+        while hex_digits[look()] do
+          get()
+        end
+      else
+        while digits[look()] or scientific_digits[look()] do
+          get()
+        end
+      end
+
+      pushToken('number')
+    end,
+    ['point'] = function()
+      for _ = 1, 2 do
+        if look() == '.' then
+          get()
+        else
+          break
+        end
+      end
+
+      local length = src:sub(start, pos - 1):len()
+      pushToken(length == 3 and 'vararg' or length == 2 and 'operator' or 'symbol')
+    end,
+    ['label'] = function()
+      get()
+
+      if #tokens - 1 > 0 and tokens[#tokens - 1].type == "label:start" then
+        pushToken('label:end')
+      else
+        pushToken('label:start')
+      end
+    end,
+    ['operator'] = function()
+      local operator = look(-1)
+      local next = look()
+
+      if arithmetic_operators[operator] and ((opts.extended_assignment and next == '=') or (operator == '/' and next == '/')) then
+        get()
+      end
+
+      if relational_operators[operator] and next == '=' then
+        get()
+      end
+
+      pushToken("operator")
+    end,
+  }
+
+  local is = {
+    ['string'] = function(char, next_char)
+      return char == '\'' or char == '"' or (char == '[' and (next_char == '[' or next_char == '='))
+    end,
+    ['word'] = function(char)
+      return alphabet[char]
+    end,
+    ['number'] = function(char, next_char)
+      return digits[char] or (char == '.' and digits[next_char])
+    end,
+    ['point'] = function(char)
+      return char == '.'
+    end,
+    ['label'] = function(char, next_char)
+      return char == ':' and next_char == ':'
+    end,
+    ['operator'] = function(char)
+      return operators[char] or char == "#"
+    end,
+    ['symbol'] = function(char)
+      return symbols[char]
+    end
+  }
+
+  while true do
+    chompWhitespace()
+    chompComment()
+
+    local char = get()
+    local next_char = look()
+
+    if char == '' then
+      break
+    end
+
+    local _ =
+        is.string(char, next_char) and tokenizer.string(char) or
+        is.word(char)              and tokenizer.word()       or
+        is.number(char, next_char) and tokenizer.number()     or
+        is.point(char)             and tokenizer.point()      or
+        is.label(char, next_char)  and tokenizer.label()      or
+        is.operator(char)          and tokenizer.operator()   or
+        is.symbol(char)            and pushToken('symbol')    or
+        pushToken('undefined')
+  end
+
+  return tokens
 end
